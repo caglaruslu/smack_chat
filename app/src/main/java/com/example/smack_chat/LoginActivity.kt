e0c57f50389c7e2df8ca62907e29347934164fdc
Caglar Uslu <caglaruslu12@gmail.com>
2019-02-18 15:54:01 +0300

    signup page

--- app/src/main/java/com/example/smack_chat/LoginActivity.kt
+++ app/src/main/java/com/example/smack_chat/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.smack_chat
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -16,6 +17,7 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginSignupButtonClicked(view: View){
-
+        val signupIntent = Intent(this, SignupActivity::class.java)
+        startActivity(signupIntent)
     }
 }
---
+++ app/src/main/java/com/example/smack_chat/SignupActivity.kt
@@ -0,0 +1,25 @@
+package com.example.smack_chat
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class SignupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+
+    fun generateUserAvatar(view: View){
+
+    }
+
+    fun generateBackgroundColorButtonClicked(view: View){
+
+    }
+
+    fun createUserButtonClicked(view: View){
+
+    }
+}
